d6a82cec91973f35f4bb8b52c4410806f42b71d1
Hennequin <pythoncraft2@gmail.com>
2021-09-21 12:42:53 +0200

    [checkpoint] protection + vie

--- src/scenes/game-scene.ts
+++ src/scenes/game-scene.ts
@@ -55,7 +55,6 @@ export class GameScene extends Phaser.Scene {
     this.setHealthBar(100)
     this.events = new Phaser.Events.EventEmitter()
     this.events.on('health-changed', this.handleHealthChanged, this)
-    this.events.on('protection-changed', this.handleProtectionChanged, this)
 
     this.count = 0;
     this.follow = true;
@@ -220,19 +219,6 @@ export class GameScene extends Phaser.Scene {
     }
   }
 
-
-  public setProtectSize(value: number) {
-    // const width = 200
-    // const percent = Phaser.Math.Clamp(value, 0, 100) / 100
-    // this.graphics.clear()
-    // this.graphics.fillStyle(0x979797)
-    // this.graphics.fillRoundedRect(10, 10, width, 20, 5).setScrollFactor(0)
-    // if (percent > 0) {
-      // this.graphics.fillStyle(0x00ff00)
-      // this.graphics.fillRoundedRect(10, 10, width * percent, 20, 5)
-    // }
-  }
-
   /**
    * Animation bar de vie + joueur quand il est attaqué
    * @param  value l'etat final de sa vie
@@ -255,21 +241,6 @@ export class GameScene extends Phaser.Scene {
     })
 
     this.lastHealth = value
-  }
-
-
-  private handleProtectionChanged(value: number) {
-    this.tweens.addCounter({
-      from: this.lastHealth,
-      to: value,
-      duration: 200,
-      ease: Phaser.Math.Easing.Sine.InOut,
-      onUpdate: tween => {
-        const value = tween.getValue()
-        this.setProtectSize(value)
-      },
-      // onComplete: () => this.girlMap.clearTint(),
-    })
   }
 
   public update(): void {
@@ -317,11 +288,11 @@ export class GameScene extends Phaser.Scene {
           if (this.ennemy.anims.getFrameName().includes("attack4")) {
 
             if (this.protect.displayWidth === 1) {
-            this.health = Phaser.Math.Clamp(this.health - 1, 0, 100)
-            this.events.emit('health-changed', this.health)
-          } else {
-            this.events.emit('protection-changed', this.health)
-          }
+              this.health = Phaser.Math.Clamp(this.health - 1, 0, 100)
+              this.events.emit('health-changed', this.health)
+            } else {
+              //Diminuer la protection
+            }
           }
         }
       } else {
